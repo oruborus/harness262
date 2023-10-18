c2be0971a5247a8a337546b0893fb2a150d31109
Felix Jahn <felix.jahn@outlook.com>
2023-10-18 12:36:07 +0200

    FIX: Add `@throws` annotations

--- src/Contracts/Assertion.php
+++ src/Contracts/Assertion.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Oru\Harness\Contracts;
 
+use Oru\Harness\Assertion\Exception\AssertionFailedException;
+use Oru\Harness\Assertion\Exception\EngineException;
+
 interface Assertion
 {
+    /**
+     * @throws AssertionFailedException
+     * @throws EngineException
+     */
     public function assert(mixed $actual): void;
 }
