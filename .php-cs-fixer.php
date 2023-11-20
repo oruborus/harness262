8bd158eb2f8b5c5f626a5a5fa7d1548a69e8bff3
Felix Jahn <felix.jahn@outlook.com>
2023-11-20 08:51:30 +0100

    FIX: Add license information to all files in `src` and `test` directory

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,10 +1,27 @@
 <?php
 
+$header = <<<EOF
+Copyright (c) 2023, Felix Jahn
+
+For the full copyright and license information, please view
+the LICENSE file that was distributed with this source code.
+
+SPDX-License-Identifier: BSD-3-Clause
+
+@see https://github.com/oruborus/harness262
+EOF;
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS2.0' => true,
+        // 'header_comment' => [
+        //     'comment_type' => 'PHPDoc',
+        //     'header' => $header,
+        //     'location' => 'after_open',
+        //     'separate' => 'both',
+        // ],
     ])
     ->setFinder($finder);
--- tests/Unit/Helpers/ErrorHandlerTest.php
+++ tests/Unit/Helpers/ErrorHandlerTest.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Copyright (c) 2023, Felix Jahn
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @see https://github.com/oruborus/harness262
+ */
+
 declare(strict_types=1);
 
 namespace Tests\Unit\Helpers;
